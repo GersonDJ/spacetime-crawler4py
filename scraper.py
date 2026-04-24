f911374bbfa094716ddc044e53e195e633bf3f08
calviw001 <calviw8@uci.edu>
2026-04-24 20:05:37 +0000

    Feature for detecting near duplicates was removed for now
    Turns out I was not supposed to import the simhash library, so I'll get back to that feature later if I have time. It's extra credit, so right now it is not a priority.

--- scraper.py
+++ scraper.py
@@ -3,7 +3,6 @@ from urllib.parse import urlparse, urljoin, urldefrag
 from bs4 import BeautifulSoup
 import lxml
 from hashlib import sha256
-from simhash import simhash, hamming_distance
 
 # Add global variables here:
 unique_urls = set()
@@ -12,7 +11,6 @@ common_word_frequencies = {}
 subdomains = {}
 
 hashes = set() # sha256
-simhash_fingerprints = set()
 
 stopwords = set([
     "a",          "about",      "above",      "after",
@@ -67,25 +65,6 @@ def is_page_duplicate(page_text):
     else:
         hashes.add(page_hash)
         return False
-
-def is_page_near_duplicate(page_words):
-    # Return true if the page is a near duplicate of a previously crawled page, and return false otherwise
-    if not page_words:
-        return False
-    
-    # Compute the fingerprint of the provided webpage words
-    curr_fingerprint = simhash(page_words, hasher="xxh3")
-
-    # Define a threshold value 
-    threshold = 6
-
-    # Check if a near duplicate page already exists using Hamming distance
-    for prev_fingerprint in simhash_fingerprints:
-        if hamming_distance(curr_fingerprint, prev_fingerprint) < threshold:
-            return True
-    
-    simhash_fingerprints.add(curr_fingerprint)
-    return False
 
 def is_too_large():
     # Return true if the file is too large, and return false otherwise
@@ -162,10 +141,6 @@ def extract_next_links(url, resp):
 
         # Store all words EXCLUDING stopwords
         content_words_no_stopwords = [word for word in all_words if word not in stopwords]
-
-        # If the webpage text is a NEAR duplicate of some previous webpage text that was already scraped, then return an empty list
-        if is_page_near_duplicate(content_words_no_stopwords):
-            return links
 
         for link in soup.find_all('a'):
             if link:
